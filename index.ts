ca4c4d86c4673e6f380cb05ab1ce86626954fea9
Hiroaki Matsuura <matsuura@tsugime.com>
2025-02-09 14:45:19 +0900

    refactoring

--- index.ts
+++ index.ts
@@ -20,67 +20,82 @@ const getValues = (data: any, at: string) => {
 export const Htma = {
     compile: (html: string, data: any = {}) => {
         data = data || {};
-        const $ = cheerio.load(html, { xmlMode: true });
 
-        // Replace text content using w-text
-        $('*[w-text]').each((_, element) => {
-            const key = $(element).attr('w-text') as string;
-            const value = getValue(data, key);
-            $(element).text(value);
-            $(element).removeAttr('w-text');
-        });
+        // Process w-for recursively
+        const processTemplate = (template: string, data: any): string => {
+            const $ = cheerio.load(template, { xmlMode: true });
 
-        // Replace HTML content using w-html
-        $('*[w-html]').each((_, element) => {
-            const key = $(element).attr('w-html') as string;
-            const value = getValue(data, key);
-            $(element).html(value);
-            $(element).removeAttr('w-html');
-        });
+            // Process w-html attributes
+            $('*[w-html]').each((_, element) => {
+                const key = $(element).attr('w-html') as string;
+                const value = getValue(data, key);
+                $(element).html(value);
+                $(element).removeAttr('w-html');
+            });
 
-        // Add new attribute using w-attr-name and w-attr-text
-        $('*[w-attr-name]').each((_, element) => {
-            const $el = $(element);
-            const attrKey = $el.attr('w-attr-name');
-            const attrText = $el.attr('w-attr-text');
-            if (attrKey && attrText) {
-                const value = getValue(data, attrText);
-                $el.attr(attrKey, value);
-                $el.removeAttr('w-attr-name');
-                $el.removeAttr('w-attr-text');
-            }
-        });
+            // Process w-text attributes
+            $('*[w-text]').each((_, element) => {
+                const key = $(element).attr('w-text') as string;
+                const value = getValue(data, key);
+                $(element).text(value);
+                $(element).removeAttr('w-text');
+            });
 
-        // Repeat elements using w-for
-        const processedForKeys = new Set<string>();
-        $('*[w-for]').each((_, element) => {
-            const $el = $(element);
-            const key = $el.attr('w-for') as string;
-            const forKey = $el.attr('w-for-key');
-            
-            if (forKey && processedForKeys.has(forKey)) {
-                $el.remove();
-                return;
-            }
-            if (forKey) {
-                processedForKeys.add(forKey);
-            }
-
-            let values = getValues(data, key);
-            if (Array.isArray(values)) {
-                const parentClone = $el.clone();
-                parentClone.removeAttr('w-for');
-                parentClone.removeAttr('w-for-key');
-                
-                for (const v of values.reverse()) {
-                    const childClone = parentClone.clone();
-                    childClone.text(v);
-                    $el.after(childClone);
+            // Process w-attr attributes
+            $('*[w-attr-name]').each((_, element) => {
+                const $el = $(element);
+                const attrKey = $el.attr('w-attr-name');
+                const attrText = $el.attr('w-attr-text');
+                if (attrKey && attrText) {
+                    const value = getValue(data, attrText);
+                    $el.attr(attrKey, value);
+                    $el.removeAttr('w-attr-name');
+                    $el.removeAttr('w-attr-text');
                 }
-            }
-            $el.remove();
-        });
+            });
 
-        return $.html();
+            // Process w-for elements
+            const processedForKeys = new Set<string>();
+            let hasForElements = false;
+
+            $('*[w-for]').each((_, element) => {
+                hasForElements = true;
+                const $el = $(element);
+                const key = $el.attr('w-for') as string;
+                const forKey = $el.attr('w-for-key');
+
+                if (forKey && processedForKeys.has(forKey)) {
+                    $el.remove();
+                    return;
+                }
+                if (forKey) {
+                    processedForKeys.add(forKey);
+                }
+
+                let values = getValues(data, key);
+                if (Array.isArray(values)) {
+                    const template = $el.clone();
+                    template.removeAttr('w-for');
+                    template.removeAttr('w-for-key');
+
+                    const results: string[] = [];
+                    for (const v of values) {
+                        const $clone = template.clone();
+                        $clone.text(v);
+                        results.push($.html($clone));
+                    }
+                    $el.replaceWith(results.join(''));
+                }
+            });
+
+            // If there are still w-for elements, process them again
+            if (hasForElements) {
+                return processTemplate($.html(), data);
+            }
+
+            return $.html();
+        };
+
+        return processTemplate(html, data);
     }
 }
